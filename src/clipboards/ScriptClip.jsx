0146cc8fdffba54fd5ae9e5f1fa83acd10c0a3dd
ej-yotpo <161460939+ej-yotpo@users.noreply.github.com>
2025-03-08 00:39:26 +0800

    Update ScriptClip.jsx

--- src/clipboards/ScriptClip.jsx
+++ src/clipboards/ScriptClip.jsx
@@ -3,7 +3,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function ScriptClipBoard() {
   const [value, setValue] = useState(`
-  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/9bo6DrWy8aIW4EJDgtFIKReJwKRs65oJfYU59YG0" async></script>
+  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/dFO0JaAqrJlob1mOb4xmRgIxml3a9KzrkO1G2zDA" async></script>
     `);
   const [isCopied, setCopied] = useState(false);
 
